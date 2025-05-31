2dc87a6eb92208b976b1d335f65adf7ebadfc171
R3v3ill3 <troy@reveille.net.au>
2025-05-31 10:41:37 +1000

    Fix unterminated string in Firebase config

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
-import serviceAccount from './pop-poll-firebase-adminsdk-fbsvc-39386d162a.json\' assert { type: "json" };
+import serviceAccount from './pop-poll-firebase-adminsdk-fbsvc-39386d162a.json' assert { type: "json" };
 
 const app = initializeApp({
   credential: cert(serviceAccount)
