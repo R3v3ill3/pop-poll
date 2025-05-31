44ee1a0d7c17bff87ceb3b78f78254d8dfda51c5
R3v3ill3 <troy@reveille.net.au>
2025-06-01 09:16:18 +1000

    Setup Frontend Environment

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2Mn4cn2KPCLee6l_fRBEpXZFZzIGh4PA",
-  authDomain: "pop-poll.firebaseapp.com",
-  projectId: "pop-poll",
-  storageBucket: "pop-poll.firebasestorage.app",
-  messagingSenderId: "1011593417699",
-  appId: "1:1011593417699:web:a3f540e9c3d8d3952e06d6",
-  measurementId: "G-7Y3ZB36TTH"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
