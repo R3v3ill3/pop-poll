7503c41d717da9fc9bfd257c306fac5c0605fd66
R3v3ill3 <troy@reveille.net.au>
2025-05-31 10:34:39 +1000

    Set up Firebase configuration

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -9,7 +9,7 @@ const serviceAccount = {
   type: "service_account",
   project_id: "pop-poll",
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY) : undefined,
+  private_key: process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY).privateKey : undefined,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
